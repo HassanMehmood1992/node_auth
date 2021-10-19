e23785f1614506a6f67059ab67a466265407d152
Hassan Mehmood <hassanmehmood@ip-192-168-178-44.eu-central-1.compute.internal>
2021-10-19 14:05:49 +0200

    authentication added for login user name and password . bcrypt

--- authServer.js
+++ authServer.js
@@ -3,11 +3,12 @@ require('dotenv').config()
 const express = require('express')
 const jwt = require('jsonwebtoken')
 const app = express()
-
+const bcrypt = require('bcrypt')
 app.use(express.json())
 
 // for demo this can be in db as it will reset on server start
 let validRefreshTokens = []
+let users = []
 
 app.post('/token', (req, res) => {
     const refreshToken = req.body.token
@@ -27,7 +28,7 @@ app.delete('/logout', (req,res)=> {
     validRefreshTokens = validRefreshTokens.filter(token => token !== req.body.token)
     res.sendStatus(204)
 })
-app.post('/login', (req, res) => {
+app.post('/login', authenticateUser, (req, res) => {
     const username = req.body.username
     const user = {
         name: username
@@ -37,6 +38,47 @@ app.post('/login', (req, res) => {
     validRefreshTokens.push(refreshToken)
     res.json({accessToken:accessToken, refreshToken:refreshToken})
 })
+
+app.get('/users', (req, res) => {
+    res.json(users)
+})
+app.post('/users', async (req, res) => {
+
+    try {
+        // const salt = await bcrypt.genSalt()
+        const hashPassword = await bcrypt.hash(req.body.password, 10)
+        const user = {
+            name: req.body.username,
+            password: hashPassword
+        }
+        users.push(user)
+        res.sendStatus(200)
+    }
+    catch {
+        req.status(500)
+    }
+
+})
+
+async function authenticateUser(req, res, next) {
+    const user = users.find(user => user.name === req.body.username)
+    console.log('user', user)
+    if (user === null || user === undefined) {
+        res.status(400).send('Cannot find user')
+        return
+    }
+    try {
+        const isAllowed = await bcrypt.compare(req.body.password, user.password)
+        if (isAllowed) {
+            next()
+        } else {
+            res.send('Not Allowed')
+        }
+    }
+    catch {
+        res.status(500).send('')
+    }
+}
 
 function generateAccessToken (user)
 {
